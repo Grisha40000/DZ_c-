b6fb89267ff2633dedc74365b46ef67d96bb90e2
grisha <grisha.zhivot@bk.ru>
2022-10-13 15:14:36 +0300

    Задача 2

--- DZ_1C#/Program.cs
+++ DZ_1C#/Program.cs
@@ -1,19 +1,37 @@
 ﻿// Задача2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// Console.Write("Введите первое число: ");
-// int A = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите второе число: ");
-// int B = Convert.ToInt32(Console.ReadLine());
+/*Console.Write("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
 
-// if(A>B)
-// {
-//     Console.Write($"Max={A} Min={B}");
-// }
-// if(A == B)
-// {
-//     Console.Write($"{A} = {B}");
-// }
-// if (A<B)
-// {
-//     Console.Write($"Max={B} Min={A}");
-// }
+if(A>B)
+{
+    Console.Write($"Max={A} Min={B}");
+}
+if(A == B)
+{
+    Console.Write($"{A} = {B}");
+}
+if (A<B)
+{
+    Console.Write($"Max={B} Min={A}");
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третие число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+ int max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.Write("Max=" + max);
+*/
