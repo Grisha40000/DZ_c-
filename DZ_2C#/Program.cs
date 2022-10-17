d6d4e226a56fa24b5996da7adc194d5fda7491e8
grisha <grisha.zhivot@bk.ru>
2022-10-17 13:08:46 +0300

    Задача 10

---
+++ DZ_2C#/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int Number2(int number)
+{
+    
+    int des2= number%100/10;
+    number=des2;
+    return number;
+}
+Console.Write("Введите трехзначное число:");
+int num=Convert.ToInt32(Console.ReadLine());
+
+int result=Number2(num);
+Console.Write($" Внесенное число {num} полученное число  {result}");
+//234
