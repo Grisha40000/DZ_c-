92c491464096fb124204d224ab08ece4b4f48444
grisha <grisha.zhivot@bk.ru>
2022-10-17 13:30:25 +0300

    Задача 15

--- DZ_2C#/Program.cs
+++ DZ_2C#/Program.cs
@@ -1,16 +1,32 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 //и на выходе показывает вторую цифру этого числа.
 
-int Number2(int number)
-{
+// int Number2(int number)
+// {
     
-    int des2= number%100/10;
-    number=des2;
-    return number;
-}
-Console.Write("Введите трехзначное число:");
-int num=Convert.ToInt32(Console.ReadLine());
+//     int des2= number%100/10;
+//     number=des2;
+//     return number;
+// }
+// Console.Write("Введите трехзначное число:");
+// int num=Convert.ToInt32(Console.ReadLine());
 
-int result=Number2(num);
-Console.Write($" Внесенное число {num} полученное число  {result}");
-//234
+// int result=Number2(num);
+// Console.Write($" Внесенное число {num} полученное число  {result}");
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+// bool Weekend(int number)
+// {
+//     if(number==6||number==7) 
+//     {
+//         return true;
+//     }
+//     else
+//     return false;
+
+// }
+// Console.Write("Введите число дня недели :");
+//  int number=Convert.ToInt32(Console.ReadLine());
+//  bool result=Weekend(number);
+//  Console.Write(result);
