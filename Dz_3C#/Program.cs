d87cfea6b4bb2e0ccead127ab0afe435b91ff418
grisha <grisha.zhivot@bk.ru>
2022-10-20 14:08:27 +0300

    Задача 23

--- Dz_3C#/Program.cs
+++ Dz_3C#/Program.cs
@@ -2,33 +2,33 @@
 // Напишите программу, которая принимает на вход координаты двух
 // точек и находит расстояние между ними в 3D пространстве.
 
-double ShowQuart(double x1 ,double x2,double y1,double y2,double z1,double z2 )
-{
-    double x =x2-x1;
-    double y =y2-y1;
-    double z =z1-z2;
+// double ShowQuart(double x1 ,double x2,double y1,double y2,double z1,double z2 )
+// {
+//     double x =x2-x1;
+//     double y =y2-y1;
+//     double z =z1-z2;
     
-    double dot=Math.Sqrt(x*x+y*y+z*z);
-    return dot;
-}
-Console.Write("Введите x1:");
-double x1=Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите y1:");
-double y1=Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите z1:");
-double z1=Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите x2:");
-double x2=Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите y2:");
-double y2=Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите z2:");
-double z2=Convert.ToDouble(Console.ReadLine());
+//     double dot=Math.Sqrt(x*x+y*y+z*z);
+//     return dot;
+// }
+// Console.Write("Введите x1:");
+// double x1=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите y1:");
+// double y1=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите z1:");
+// double z1=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите x2:");
+// double x2=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите y2:");
+// double y2=Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите z2:");
+// double z2=Convert.ToDouble(Console.ReadLine());
 
-double res=ShowQuart(x1,x2,y1,y2,z1,z2);
-Console.Write("Растоение ="+ res);
+// double res=ShowQuart(x1,x2,y1,y2,z1,z2);
+// Console.Write("Растоение ="+ res);
 
-// Напишите программу, которая принимает на вход число (N) 
-// и выводит квадраты чисел от 1 до N.
+// Задача 23 Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
 
 // void Show(int N)
 // {
@@ -37,7 +37,7 @@ Console.Write("Растоение ="+ res);
 //     while (index<=N)
 //     {
         
-//         Console.Write(index*index +" ");
+//         Console.Write(index*index*index +" ");
 //         index++;
 //     }
 // }
